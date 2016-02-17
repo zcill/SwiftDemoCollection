d272821189c300bb57bf21c0875f988049236b66
zcill <zcillcoder@gmail.com>
2016-02-18 00:07:07 +0800

    add Demo2-ChangeFont, a simple ViewController with a button can change font.

---
+++ Demo2-ChangeFont/Demo2-ChangeFont/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo2-ChangeFont
+//
+//  Created by 朱立焜 on 16/2/17.
+//  Copyright © 2016年 朱立焜. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
